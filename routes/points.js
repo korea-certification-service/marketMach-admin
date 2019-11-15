4d12fdf2baad5c7a6f6b2891e100a6b937758967
dongil Kim <kdi@onlykcs.com>
2019-11-15 14:17:41 +0900

    - 포인트 거래 내역 조회 개선

--- routes/points.js
+++ routes/points.js
@@ -2,13 +2,49 @@ var express = require('express');
 var router = express.Router();
 var points = require('../backend/controller/points');
 var sessionChecker = require('../utils/session');
+let BitwebResponse = require('../utils/BitwebResponse');
+var dbconfig = require('../config/dbconfig');
+var request = require('request');
 
 router.get('/lists', sessionChecker.sessionChecker, function(req, res, next) {
     res.render('points/trade', { title: 'Biteweb Admin - PointTrades' });
 });
 
 router.post('/search', function(req, res, next) {
-    points.listPointTrade(req, res);
+    //points.listPointTrade(req, res);
+    let bitwebResponse = new BitwebResponse();
+    let url = dbconfig.APIServer + "/ma_points/list/all";
+    let header = {
+        'loginToken': req.cookies.loginToken,
+        'token': dbconfig.APIToken
+    };
+    let body = req.body;
+    let reqs = {uri: url, 
+        method:'POST',
+        headers: header,
+        body:body,
+        json: true
+    }
+
+    //채팅 서버에서 API 서버로 내부 call요청한다.
+    request(reqs, function (error, response, body) {  
+        console.log(error, response, body);
+        if (!error && response.statusCode == 200) {
+            let result = body.data;
+            if(typeof(body) == "string") {
+                result = JSON.parse(body).data;
+            }
+            
+            bitwebResponse.code = 200;
+            bitwebResponse.data = result;
+            res.status(200).send(bitwebResponse.create())
+        } else {
+            console.error('err=>', error)
+            bitwebResponse.code = 500;
+            bitwebResponse.message = error;
+            res.status(500).send(bitwebResponse.create())
+        }
+    });
 });
 
 router.get('/deposits/lists', sessionChecker.sessionChecker, function(req, res, next) {
